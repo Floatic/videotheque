3d752767a82f06139c026521592897876873147d
Florian <florian.grignoux@outlook.fr>
2015-07-08 14:56:24 +0200

    Intégration ajout de vidéo

--- workspace/client/app/ajouter-video/ajouter-video.controller.js
+++ workspace/client/app/ajouter-video/ajouter-video.controller.js
@@ -3,8 +3,16 @@
 angular.module('videothequeApp')
         .controller('AjouterVideoCtrl', function ($scope, FileUploader) {
             var uploader = $scope.uploader = new FileUploader({
-                url : '/api/videos'
+                url: '/api/videos'
             });
+
+            // Button event
+            $scope.upload = function(){
+                console.log('click');
+                setTimeout(function () {
+                  angular.element('#upload').trigger('click');
+                }, 0);
+            };
 
             // CALLBACKS
 
@@ -48,3 +56,33 @@ angular.module('videothequeApp')
 //            console.info('uploader', uploader);
 
         });
+//        .directive('myTest', function () {
+//            return {
+//                link: function (scope, element, attr) {
+//                    element.on('click', function (event) {
+//                        var button = angular.element(element);
+////console.log(el);
+//                        el.css({
+//                            position: 'relative',
+//                            overflow: 'hidden',
+//                            width: button.offsetWidth,
+//                            height: button.offsetHeight
+//                        });
+//
+//                        var fileInput = angular.element('<input type="file" />');
+//                        fileInput.css({
+//                            position: 'absolute',
+//                            top: 0,
+//                            left: 0,
+//                            'z-index': '2',
+//                            width: '100%',
+//                            height: '100%',
+//                            opacity: '0',
+//                            cursor: 'pointer'
+//                        });
+//
+//                        button.append(fileInput);
+//                    });
+//                }
+//            };
+//        });
